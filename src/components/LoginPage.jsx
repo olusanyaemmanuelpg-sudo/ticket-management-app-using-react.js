e2234809fc65b993f0dd763969cadb6fc7d6eacd
Olusanya Emmanuel <olusanyaemmanuelpg@gmailcom>
2025-10-24 03:17:44 +0100

    Created the login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -5,7 +5,26 @@ import './LoginPage.css';
 export function LoginPage() {
 	return (
 		<section className='loginSection'>
-			<div className='loginPage'></div>
+			<div className='loginPage'>
+				<h2>Welcome Back</h2>
+				<p>Login to your account</p>
+
+				<div className='login-input'>
+					<label>Email</label>
+					<input type='email' placeholder='admin@ticket.com' />
+				</div>
+				<div className='login-input'>
+					<label>Password</label>
+					<input type='password' placeholder='password123' />
+				</div>
+
+				<button className='login-btn'>Login</button>
+				<p className='login-signup'>
+					Don't have an account?
+					<a href=''> Sign up</a>
+				</p>
+				<button className='back-to-home'>Back to Home</button>
+			</div>
 		</section>
 	);
 }
